52140371a499a22c4808b903ad790a13f2be9496
PatrickDdx <213446929+PatrickDdx@users.noreply.github.com>
2026-07-10 19:08:34 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Find the button element by its id
+const button = document.getElementById('click-me');
+
+// Add a click handler
+button.addEventListener('click', () => {
+  alert('JavaScript is linked and working!');
+});
